e7d97778b4ca7405cf1704b114de62b60e14a14a
Valeria Protsan <valeriyapro@gmail.com>
2018-06-17 12:31:57 -0400

    messign with webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App'
+require('./style.css');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const favicon = require('./assets/favicon.png');
+let link = document.createElement('link');
+link.type = 'image/png';
+link.rel = 'shortcut icon';
+link.href = favicon;
+document.head.appendChild(link);
+
+let title = document.createElement('h1');
+title.innerText = 'Hello Wepack';
+document.body.appendChild(title);
