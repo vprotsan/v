a09152994f666d903187f3bc281fd4daa01646d6
Valeria Protsan <valeriyapro@gmail.com>
2018-06-18 12:37:32 -0400

    c

--- src/fragments/Header/index.js
+++ src/fragments/Header/index.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router-dom'
 import handleClickAway from '../../utils/handleClickAway'
+import Logo from '../Logo'
 import styles from './Header.css'
 import classnames from 'classnames'
 import logo from '../../assets/serverless-logo.svg';
@@ -13,18 +14,7 @@ export default class Header extends Component{
     return(
       <header id={styles.header} ref={header => { this.header = header }}>
           <div className={styles.container}>
-            <Link to='/' className={styles.logo}>
-              <img
-                id='logo'
-                src={logo}
-                width={177}
-                height={30}
-                ref={logo => { this.logo = logo }}
-              />
-              <span className={styles.logoText}>
-                serverless
-              </span>
-            </Link>
+            <Logo />
             <div ref='toggle' className={styles.toggle}>
               <div className={styles.ham}>
                 <div className={`${styles.bar}`} />
